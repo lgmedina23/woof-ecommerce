7ecf510f9e8b49917c3ea0f13ef73aa12af5b894
Luis Guillermo <abg.lgmm@gmail.com>
2023-12-14 15:46:43 +0000

    done done

--- src/front/js/component/BillsCard.jsx
+++ src/front/js/component/BillsCard.jsx
@@ -6,6 +6,9 @@ import { Context } from "../store/appContext.js";
 
 export const BillsCard = (props) => {
     const { store, actions } = useContext(Context);
+    // const currentProduct = store.products.filter((product) => product.id == props.bill.bill_items[0].product_id)
+    const pruebita = props.bill.bill_items[0].product_id
+    console.log(pruebita);
 
 
     return (
@@ -13,6 +16,15 @@ export const BillsCard = (props) => {
             <div className="card text-dark border-primary">
                 <div className="card-header fw-bolder bg-primary-subtle">Número de factura: {props.bill.id}</div>
                 <ul className="list-group list-group-flush text-start">
+                    {props.bill.bill_items.map( (element, index) => {
+                        const currentProduct = store.products.filter((product) => product.id == element.product_id)
+                        return ( 
+                            <li  key={index} className="list-group-item text-dark d-flex justify-content-between">
+                                <span className="fw-medium">Nombre: {currentProduct[0].name} </span>
+                                <span className="fw-medium">Cant.: {element.quantity} </span>
+                            </li>
+                        )
+                    })}
                     <li className="list-group-item text-dark"><span className="fw-medium">Productos: {props.bill.bill_items.length}</span></li>
                     <li className="list-group-item text-dark"><span className="fw-medium">Precio total: </span>{props.bill.total_price}€</li>
                     <li className="list-group-item text-warning"><span className="fw-medium text-dark">Estatus: </span>{props.bill.status}</li>
--- src/front/js/component/BillsFlow.jsx
+++ src/front/js/component/BillsFlow.jsx
@@ -9,9 +9,9 @@ export const BillsFlow = () => {
 
     return (
         <div className="container text-center">
-            <div className="row row-cols-lg-7 row-cols-sm-2 row-cols-xs-2 row-cols-xl-5">
+            <div className="row row-cols-lg-3 row-cols-sm-1 row-cols-md-2 row-cols-xl-4">
                 {store.bills.map((bill) => (
-                    <div className="col col-lg-4" key={bill.id}>
+                    <div className="col" key={bill.id}>
                         <div className="p-3">
                             <BillsCard bill={bill} id={bill.id} />
                         </div>
