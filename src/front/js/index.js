03612f39d11aed4f0a8789f97b1104894e51d030
Luis Guillermo <lgmedina23@lgmedina23.local>
2024-04-11 17:54:02 +0200

    trying to deploy2

--- src/front/js/index.js
+++ src/front/js/index.js
@@ -4,6 +4,5 @@ import 'bootswatch/dist/minty/bootstrap.min.css';
 import "../styles/index.css";  // Include your index.scss file into the bundle
 import Layout from "./Layout.jsx";  // Import your own components
 
-require('dotenv').config()
 // Render your react application
 ReactDOM.render(<Layout />, document.querySelector("#app"));
